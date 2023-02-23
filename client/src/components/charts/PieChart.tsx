a640fd7e5e52d4601142c714c4f11f977fbdeb80
Zaid-maker <pzhafeez@gmail.com>
2023-02-23 18:35:19 +0500

    update PieChart.tsx and home.tsx

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -5,7 +5,21 @@ import { PieChartProps } from "interfaces/home";
 
 const PieChart = ({ title, value, series, colors }: PieChartProps) => {
   return (
-    <Box>
+    <Box
+      id="chart"
+      flex={1}
+      display="flex"
+      bgcolor="#fcfcfc"
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      pl={3.5}
+      py={2}
+      gap={2}
+      borderRadius="15px"
+      minHeight="110px"
+      width="fit-content"
+    >
       <Stack direction="column">
         <Typography fontSize={14} color="#808191">
           {title}
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -23,13 +23,13 @@ const home = () => {
           title="Properties for Sale"
           value={684}
           series={[75, 25]}
-          colors={["#475be8", "#e4e8ef"]}
+          colors={["#275be8", "#c4e8ef"]}
         />
         <PieChart
           title="Properties for Rent"
           value={550}
           series={[60, 40]}
-          colors={["#475ae8", "#e4b8ef"]}
+          colors={["#275be8", "#c4e8ef"]}
         />
         <PieChart
           title="Total Customers"
@@ -41,7 +41,7 @@ const home = () => {
           title="Properties for Cities"
           value={555}
           series={[75, 25]}
-          colors={["#475be8", "#e4e8ef"]}
+          colors={["#275be8", "#c4e8ef"]}
         />
       </Box>
     </Box>
